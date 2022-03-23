e11957a631b2419e275e945e8bd108ee957ecfb1
unknown <gsumitg673@gmail.com>
2022-03-23 18:26:28 +0530

    find param and added payload,finding way to add many payload

--- Multithreading.py
+++ Multithreading.py
@@ -1,17 +1,20 @@
 from threading import Thread 
 from URLSearchParams import URLSearchParams
+from lxml import etree
 
 
 def webscrap(url):
     import requests 
     from bs4 import BeautifulSoup
-    url = input("enter the url:")
     content = requests.get(url)
     html_content = content.content
     soup = BeautifulSoup(html_content,'html.parser')
-    print(soup)
+    print(soup.prettify())
+    dom = etree.HTML(str(soup))
+    print(dom.xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')[0].text)
 
 
+webscrap("https://www.google.com")
 
 
 
--- webscrapping.py
+++
@@ -1,7 +0,0 @@
-import requests 
-from bs4 import BeautifulSoup
-url = input("enter the url:")
-content = requests.get(url)
-html_content = content.text
-soup = BeautifulSoup(html_content,'html.parser')
-print(soup.prettify())
